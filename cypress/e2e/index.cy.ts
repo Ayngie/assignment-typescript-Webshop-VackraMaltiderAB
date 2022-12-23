2a267af6c771e32d91f69ffbad29ee70791c3b81
Angelica Reuterswärd <angelica@macbook-pro.lan>
2022-12-23 16:57:50 +0100

    Påbörjat cypress E2E testning av startpage

---
+++ cypress/e2e/index.cy.ts
@@ -0,0 +1,24 @@
+describe("startpage", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:1234/"); //ladda om sidan
+  });
+
+  it("should get the indexpage's title", () => {
+    cy.title().should("include", "DukaFint AB");
+  });
+
+  it("should find image", () => {
+    cy.get("#welcome-image")
+      .find("img")
+      .should("have.attr", "alt")
+      .should("include", "Juldukning från Mateus");
+
+    cy.get("#welcome-image")
+      .find("img")
+      .should(
+        "have.attr",
+        "src",
+        "https://mateus-images.imgix.net/Tablesettings/mateus_jul_mm00770_sRGB.jpg"
+      );
+  });
+});
